59f2e50921a2c3118eb5954823da3d5927caa978
Ulrich Petri <u.petri@diefirma.de>
2009-10-09 19:09:47 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-reversetag",
+    version = "0.2",
+    packages = find_packages(),
+    author = "Ulrich Petri",
+    author_email = "mail@ulo.pe",
+    description = "A new, improved (and renamed) implementation of Django's 'url' template tag.",
+    long_description = """
+    """,
+    license = "BSD",
+    keywords = "django",
+    url = "http://github.com/ulope/django-reversetag",
+)
+
