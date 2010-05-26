617bd6c6c687a375eff6c59c055a24aec7a381b1
Ulrich Petri <mail@ulo.pe>
2010-05-26 16:57:52 +0200

    Bumped version. Changed license to actually match LICENSE file

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-reversetag",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages(),
     author = "Ulrich Petri",
     author_email = "mail@ulo.pe",
-    description = "A new, improved (and renamed) implementation of Django's 'url' template tag.",
+    description = "A new, improved (and renamed) implementation of " \
+                  "Django's 'url' template tag.",
     long_description = """
     """,
-    license = "BSD",
+    license = "MIT",
     keywords = "django",
     url = "http://github.com/ulope/django-reversetag",
 )
