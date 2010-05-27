8e6587a92c38a68ae9068d9cc0c7cc32ef986aad
Ulrich Petri <mail@ulo.pe>
2010-05-27 15:31:23 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-reversetag",
-    version = "0.3",
+    version = "0.3.1",
     packages = find_packages(),
     author = "Ulrich Petri",
     author_email = "mail@ulo.pe",
